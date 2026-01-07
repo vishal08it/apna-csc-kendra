10c0fa9a0d7a34b796ae43e25463626d0012e33b
vishalraj08it <vishalraj08it@gmail.com>
2026-01-07 19:13:25 +0530

    CLODINARY AND GMAIL ADDED

---
+++ app/api/send-mail/upload/route.ts
@@ -0,0 +1,37 @@
+import cloudinary from "@/lib/cloudinary";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
+
+    if (!file) {
+      return Response.json({ message: "No file uploaded" }, { status: 400 });
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const uploadResult: any = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream(
+        {
+          folder: "apna-csc-documents",
+          resource_type: "image",
+        },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      ).end(buffer);
+    });
+
+    return Response.json({
+      success: true,
+      url: uploadResult.secure_url,
+    });
+
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: "Upload failed" }, { status: 500 });
+  }
+}
