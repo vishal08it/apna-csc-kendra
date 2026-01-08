e167c9de6c70fb0f96f100c77813a1feb7a9c0b1
vishalraj08it <vishalraj08it@gmail.com>
2026-01-08 16:21:24 +0530

    monodb added

---
+++ lib/mongodb.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("❌ MONGODB_URI is not defined in .env");
+}
+
+/**
+ * Global cache to prevent multiple connections in dev
+ */
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
