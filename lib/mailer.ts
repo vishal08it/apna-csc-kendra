7d2e71ddbfb977267018e904776affc364437345
vishalraj08it <vishalraj08it@gmail.com>
2026-01-13 16:57:32 +0530

    mailer

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,9 +1,21 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+export async function sendMail({ to, subject, text, attachments }: any) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Apna CSC" <${process.env.MAIL_USER}>`,
+    to,
+    subject,
+    text,
+    attachments,
+  });
+}
